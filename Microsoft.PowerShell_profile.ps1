0ab09ca707926ee2e165c00589570798c5e186c6
José Carlos Gutiérrez <joseca20025@gmail.com>
2025-03-11 21:07:05 -0400

    Añadir configuración de PowerShell y temas de Oh My Posh

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,20 @@
+# amro.omp.json | pure.omp.json
+$omp_config = Join-Path $PSScriptRoot ".\omp-themes\amro.omp.json"
+
+# Initialize oh-my-posh
+oh-my-posh init pwsh --config $omp_config | Invoke-Expression
+
+# Import Terminal-Icons module
+Import-Module -Name Terminal-Icons
+
+function GoDevFunc {
+    Set-Location -Path "D:\dev"    
+}
+
+Set-Alias -Name godev -Value GoDevFunc
+
+function GoKaisFunc {
+    Set-Location -Path "D:/dev/work/kais"    
+}
+
+Set-Alias -Name gokais -Value GoKaisFunc
---
+++ README.md
@@ -0,0 +1,6 @@
+# Configuraciones
+Repositorio de mis configuraciones de terminal, editor de texto, etc.
+
+## Powershell
+1. Copiar el archivo `Microsoft.PowerShell_profile.ps1` en la carpeta `C:\Users\<Usuario>\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1`.
+2. Cambiar la ruta de los temas para coincidir con la carpeta donde se encuentran los temas. (`omp-themes`)
